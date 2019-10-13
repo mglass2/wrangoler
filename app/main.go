087649224671ac0ccb824dedf1853cf227ee552f
mglass <mglass2@gmail.com>
2019-10-13 01:13:37 -0400

    more restructuring

--- app/main.go
+++ app/main.go
@@ -8,17 +8,7 @@ import (
 )
 
 func main() {
-
-  var cmdEcho = &cobra.Command{
-    Use:   "echo [string to echo]",
-    Short: "Echo anything to the screen",
-    Long: `echo is for echoing anything back.
-Echo works a lot like print, except it has a child command.`,
-    Args: cobra.MinimumNArgs(1),
-    Run: command.Echo,
-  }
-
   var rootCmd = &cobra.Command{Use: "app"}
-  rootCmd.AddCommand(cmdEcho)
+  rootCmd.AddCommand(command.Echo)
   rootCmd.Execute()
 }
--- app/src/command/echoCommand.go
+++ app/src/command/echoCommand.go
@@ -5,7 +5,20 @@ import (
     "strings"
     "github.com/spf13/cobra"
 )
+var source string
 
-func Echo(cmd *cobra.Command, args []string) {
+var Echo = &cobra.Command{
+    Use:   "echo [string to echo]",
+    Short: "Echo anything to the screen",
+    Args: cobra.MinimumNArgs(1),
+    Run: action,
+}
+
+func init() {
+    Echo.Flags().StringVarP(&source, "source", "s", "", "Source directory to read from")
+}
+
+func action(cmd *cobra.Command, args []string) {
     fmt.Println("Print: " + strings.Join(args, " "))
 }
+
